377b7528d99a96a9fd7ad97db893ece8aff38db3
generic-name-2166 <kirbesson277@mail.ru>
2024-11-16 18:19:27 +0300

    feat(front): buttons

--- frontend/src/app/add/page.tsx
+++ frontend/src/app/add/page.tsx
@@ -9,10 +9,22 @@ import Cross from "@/lib/Cross.tsx";
 export default function AddPage(): JSX.Element {
   return (
     <form className="flex h-screen flex-col space-y-5 bg-gray-300">
-      <h1 className="px-4 pt-4 font-bold text-lg flex flex-row justify-between"><span>Adding a product</span> <Link href="/"><Cross /></Link></h1>
+      <h1 className="flex flex-row justify-between px-4 pt-4 font-bold text-lg">
+        <span>Adding a product</span>{" "}
+        <Link href="/">
+          <Cross />
+        </Link>
+      </h1>
       <MainForm />
       <ImageForm />
-      <button type="submit">Add product</button>
+      <div className="flex w-full grow justify-center bg-white px-5 py-1 align-bottom">
+        <button
+          type="submit"
+          className="h-fit grow rounded-2xl bg-blue-500 py-3 font-bold text-white"
+        >
+          Add product
+        </button>
+      </div>
     </form>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,15 @@ import type { JSX } from "react";
 
 export default function Home(): JSX.Element {
   return (
-    <main>
-      <Link href="/add">Add a product</Link>
+    <main className="flex">
+      <p className="flex w-full justify-center p-2">
+        <Link
+          href="/add"
+          className="flex w-full justify-center rounded-2xl bg-blue-500 py-2 text-white"
+        >
+          Add a product
+        </Link>
+      </p>
     </main>
   );
 }
